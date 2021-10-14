23077c60d31a106e77c3886aa945e32b57af64ca
jhernand1 <ahjesus@cuc.edu.co>
2021-10-14 08:52:07 -0500

    se agrega el proyecto

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app =Flask(__name__)
+
+@app.route("/mis-habitaciones",methods=['GET'])
+def mishabitaciones():
+    return render_template("gestion_comentarios.html")
+    
+@app.route("/")
+def crearhabitacion():
+    return render_template("Agregar_habitacion.html") 
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
